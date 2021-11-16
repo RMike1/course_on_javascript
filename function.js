bdc86aa87e045dfdf4ab0b5153e80a45c512e54b
rmike1 <rudasingwamike@outlook.com>
2021-11-16 12:04:03 +0200

    adding functions file..

---
+++ function.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Functions</h2>
+
+<p>javascript functions..</p>
+
+<p id="demo"></p>
+
+<script>
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("demo").innerHTML = myFunction(6, 73);
+</script>
+
+</body>
+</html>
